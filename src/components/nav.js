e0d9ccdd3dc0ad8c4854ddb91f31e72109ac5356
tosebikan <tosebikan@gmail.com>
2021-07-01 00:40:02 +0000

    fix all screens & nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -11,152 +11,147 @@ function Nav() {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/" exact>
+          <NavLink to="/contact" exact>
             Contact
           </NavLink>
         </li>
         <li>
-          <NavLink to="/" exact>
+          <NavLink to="/about" exact>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink to="/" exact>
-            Our work
-          </NavLink>
+          Our work
           <ul>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/policy" exact>
                 Policy Development Input Support
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/equality" exact>
                 Equality Diversity and Inclusion
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/legal" exact>
                 Legal Services
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/projects" exact>
                 Projects
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/strategic" exact>
                 Strategic Partnerships
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/community" exact>
                 Community Engagements
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/opportunity" exact>
                 Creating Opportunities
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
-          <NavLink to="/" exact>
-            Resources
-          </NavLink>
+          Resources
           <ul>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/reports" exact>
                 Reports
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/links" exact>
                 Links
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/info" exact>
                 Helpful information
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/financial-accounts" exact>
                 Financial Accounts
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
-          <NavLink to="/" exact>
-            Get involved
-          </NavLink>
-
+          Get involved
           <ul>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/donate" exact>
                 Donate
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/volunteer" exact>
                 Volunteer
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/fund-raising" exact>
                 Fundraising
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/jobs" exact>
                 Jobs
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
-          <NavLink to="/" exact>
-            Media
-          </NavLink>
+          Media
           <ul>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/gallery" exact>
                 Gallery (Photo and videos)
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/podcast" exact>
                 Podcast
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/blog" exact>
                 Blog
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/news" exact>
                 News
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
-          <NavLink to="/" exact>
+          <NavLink to="/events" exact>
             Events
           </NavLink>
           <ul>
             <li>
-              <NavLink to="/" exact>
+              <NavLink to="/upcoming_events" exact>
                 Upcoming Events
               </NavLink>
-              <NavLink to="/" exact>
+            </li>
+            <li>
+              <NavLink to="/past-events" exact>
                 Past Events
               </NavLink>
-              <NavLink to="/" exact>
+            </li>
+            <li>
+              <NavLink to="/speak" exact>
                 Speak at our Events
               </NavLink>
             </li>
