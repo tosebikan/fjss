9bb2ddfd18891956be05abe46672629dfa625fbd
tosebikan <tosebikan@gmail.com>
2021-07-09 14:22:26 +0000

    finifh policy screen

--- src/screens/work/policy.js
+++ src/screens/work/policy.js
@@ -10,6 +10,25 @@ function Policy() {
           <p>FJSS GROUP</p>
         </div>
       </div>
+      {/*POLICY DEV */}
+      <div className="policy_dev">
+        <h2>Policy Development and Support</h2>
+        <p>
+          The FJSS Group provides policy development input and support to
+          institutions and organisations that do not have adequate BAME
+          representation in their key decision-making structures. This helps the
+          organisations to bridge the racial equality gap through the input they
+          get from our policy experts. In 2020 the organisation commenced
+          working with the Scottish Government to provide them with essential
+          BAME input in their policy development processes and give BAME people
+          a voice in policy and key decision making. The work with the Scottish
+          Government is focused on the justice sector, ensuring issues are
+          raised in the right arena for change, and providing a platform for
+          public consultation and engagement to represent these unheard voices.
+          There is scope to expand this work to cover other areas of the
+          Scottish Government departments.
+        </p>
+      </div>
     </div>
   );
 }
