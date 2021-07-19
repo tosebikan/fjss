898d3018b7816545b7b4df0507e110cae8e7e5c3
tosebikan <tosebikan@gmail.com>
2021-07-19 09:24:32 +0000

    add approach screen

--- src/screens/about/approach.js
+++ src/screens/about/approach.js
@@ -1,7 +1,19 @@
+import "./approach.css";
+import HeroImage from "../../assets/images/home_hero.jpg";
 function Approach() {
   return (
     <div className="approach_container">
-      <h2>Welcome to Approach Screen</h2>
+      <div className="approach_hero">
+        <img src={HeroImage} alt="" />
+        <div className="approach_hero_text">
+          <h2>Our Approach</h2>
+          <p>FJSS GROUP</p>
+        </div>
+      </div>
+
+      {/*TOP SECTION */}
+
+      <div></div>
     </div>
   );
 }
