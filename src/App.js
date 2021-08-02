db7966106f157b42a43d36b9dbd48c404ddbb5eb
tosebikan <tosebikan@gmail.com>
2021-08-02 17:47:49 +0000

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -44,86 +44,94 @@ import Footer from "./components/footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import ScrollToTop from "./components/scrollToTop";
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header"></header>
-        <Nav />
+      <ScrollToTop>
+        <div className="App">
+          <header className="App-header"></header>
+          <Nav />
 
-        <Switch className="routes">
-          <Route path="/" component={Home} exact={true} />
+          <Switch className="routes">
+            <Route path="/" component={Home} exact={true} />
 
-          {/* About */}
-          <Route path="/about" component={About} exact={true} />
-          <Route path="/approach" component={Approach} exact={true} />
-          <Route path="/people" component={People} exact={true} />
+            {/* About */}
+            <Route path="/about" component={About} exact={true} />
+            <Route path="/approach" component={Approach} exact={true} />
+            <Route path="/people" component={People} exact={true} />
 
-          {/* Auth */}
-          <Route path="/login" component={Login} exact={true} />
-          <Route path="/register" component={Register} exact={true} />
+            {/* Auth */}
+            <Route path="/login" component={Login} exact={true} />
+            <Route path="/register" component={Register} exact={true} />
 
-          {/* Contact */}
-          <Route path="/contact" component={Contact} exact={true} />
+            {/* Contact */}
+            <Route path="/contact" component={Contact} exact={true} />
 
-          {/* Events */}
-          <Route path="/events" component={Events} exact={true} />
-          <Route path="/event-details" component={EventDetails} exact={true} />
-          <Route path="/past-events" component={PastEvents} exact={true} />
-          <Route
-            path="/upcoming_events"
-            component={UpcomingEvents}
-            exact={true}
-          />
-          <Route path="/speak" component={Speak} exact={true} />
+            {/* Events */}
+            <Route path="/events" component={Events} exact={true} />
+            <Route
+              path="/event-details"
+              component={EventDetails}
+              exact={true}
+            />
+            <Route path="/past-events" component={PastEvents} exact={true} />
+            <Route
+              path="/upcoming_events"
+              component={UpcomingEvents}
+              exact={true}
+            />
+            <Route path="/speak" component={Speak} exact={true} />
 
-          {/* Involved*/}
-          <Route path="/donate" component={Donate} exact={true} />
-          <Route path="/fund-raising" component={FundRaising} exact={true} />
-          <Route path="/involved" component={Involved} exact={true} />
-          <Route path="/jobs" component={Jobs} exact={true} />
-          <Route path="/volunteer" component={Volunteer} exact={true} />
+            {/* Involved*/}
+            <Route path="/donate" component={Donate} exact={true} />
+            <Route path="/fund-raising" component={FundRaising} exact={true} />
+            <Route path="/involved" component={Involved} exact={true} />
+            <Route path="/jobs" component={Jobs} exact={true} />
+            <Route path="/volunteer" component={Volunteer} exact={true} />
 
-          {/*Media */}
-          <Route path="/blog" component={Blog} exact={true} />
-          <Route path="/gallery" component={Gallery} exact={true} />
-          <Route path="/news-details" component={NewsDetails} exact={true} />
-          <Route path="/news" component={News} exact={true} />
-          <Route path="/podcast" component={Podcast} exact={true} />
+            {/*Media */}
+            <Route path="/blog" component={Blog} exact={true} />
+            <Route path="/gallery" component={Gallery} exact={true} />
+            <Route path="/news-details" component={NewsDetails} exact={true} />
+            <Route path="/news" component={News} exact={true} />
+            <Route path="/podcast" component={Podcast} exact={true} />
 
-          {/*Resources */}
-          <Route
-            path="/financial-accounts"
-            component={FinancialAccounts}
-            exact={true}
-          />
-          <Route path="/info" component={Info} exact={true} />
-          <Route path="/links" component={Links} exact={true} />
-          <Route path="/reports" component={Reports} exact={true} />
+            {/*Resources */}
+            <Route
+              path="/financial-accounts"
+              component={FinancialAccounts}
+              exact={true}
+            />
+            <Route path="/info" component={Info} exact={true} />
+            <Route path="/links" component={Links} exact={true} />
+            <Route path="/reports" component={Reports} exact={true} />
 
-          {/*Shop */}
-          <Route path="/shop" component={Shop} exact={true} />
-          <Route path="/cart" component={Cart} exact={true} />
+            {/*Shop */}
+            <Route path="/shop" component={Shop} exact={true} />
+            <Route path="/cart" component={Cart} exact={true} />
 
-          {/* Work */}
-          <Route path="/community" component={Community} exact={true} />
-          <Route path="/equality" component={Equality} exact={true} />
-          <Route path="/legal" component={Legal} exact={true} />
-          <Route path="/opportunity" component={Opportunity} exact={true} />
-          <Route path="/policy" component={Policy} exact={true} />
-          <Route path="/projects" component={Projects} exact={true} />
-          <Route
-            path="/project-details"
-            component={ProjectDetails}
-            exact={true}
-          />
-          <Route path="/strategic" component={Strategic} exact={true} />
+            {/* Work */}
+            <Route path="/community" component={Community} exact={true} />
+            <Route path="/equality" component={Equality} exact={true} />
+            <Route path="/legal" component={Legal} exact={true} />
+            <Route path="/opportunity" component={Opportunity} exact={true} />
+            <Route path="/policy" component={Policy} exact={true} />
+            <Route path="/projects" component={Projects} exact={true} />
+            <Route
+              path="/project-details"
+              component={ProjectDetails}
+              exact={true}
+            />
+            <Route path="/strategic" component={Strategic} exact={true} />
 
-          {/*PRIVACY POLICY*/}
-          <Route path="/privacy" component={PrivacyPolicy} exact={true} />
-        </Switch>
-      </div>
-      <Footer />
+            {/*PRIVACY POLICY*/}
+            <Route path="/privacy" component={PrivacyPolicy} exact={true} />
+          </Switch>
+        </div>
+        <Footer />
+      </ScrollToTop>
     </Router>
   );
 }
