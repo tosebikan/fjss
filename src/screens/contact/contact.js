88796e36284135ca9134bc0f86c7ec6bf8dd714c
tosebikan <tosebikan@gmail.com>
2021-08-02 08:12:54 +0000

    finish contact form

--- src/screens/contact/contact.js
+++ src/screens/contact/contact.js
@@ -1,3 +1,4 @@
+import * as React from "react";
 import "./contact.css";
 import HeroImage from "../../assets/images/home_hero.jpg";
 // import gallery1 from "../../assets/images/gallery1.png";
@@ -8,15 +9,29 @@ import axios from "axios";
 import emailjs from "emailjs-com";
 
 function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [response, setResponse] = React.useState("");
+
   const sendMail = async e => {
     e.preventDefault();
 
+    console.log({ name, email, message });
+    if (name.length === 0 || email.length === 0 || message.length === 0) {
+      alert("please enter required fields");
+      return;
+    }
+
+    setLoading(true);
+
     let template_params = {
-      from_name: "Tolu",
+      from_name: name,
       to_name: "FJSS GROUP",
-      message: "testing emailjs, the template has been updated",
+      message: message,
       to: "tosebikan@gmail.com",
-      from_mail: "oltostudios@gmail.com"
+      from_mail: email
     };
 
     window.emailjs
@@ -24,12 +39,20 @@ function Contact() {
       .then(
         function(response) {
           console.log("SUCCESS!", response.status, response.text);
+          setLoading(false);
+          setMessage("");
+          setName("");
+          setEmail("");
+          setResponse("SUCCESS!");
         },
         function(error) {
           console.log("FAILED...", error);
+          setLoading(false);
+          setResponse("FAILED..., please try again later");
         }
       );
   };
+
   return (
     <div className="contact_container">
       <div className="contact_hero">
@@ -95,13 +118,41 @@ function Contact() {
 
         <form className="contact_form">
           <div className="contact_form_row">
-            <input placeholder="Your Name*" name="user_name" />
-            <input placeholder="Your Email*" name="user_email" />
+            <input
+              placeholder="Your Name*"
+              name="user_name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <input
+              placeholder="Your Email*"
+              name="user_email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
           </div>
           <input placeholder="How did you hear about us?" />
           <input placeholder="Subject*" />
-          <textArea placeholder="Leave a Messsage*" name="message" />
-          <button onClick={sendMail}>Send Message</button>
+          <textarea
+            placeholder="Leave a Messsage*"
+            name="message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          {loading ? (
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+              alt=""
+              style={{
+                width: 100,
+                height: 100,
+                objectFit: "contain",
+                alignSelf: "center"
+              }}
+            />
+          ) : (
+            <button onClick={sendMail}>Send Message</button>
+          )}
         </form>
       </div>
     </div>
