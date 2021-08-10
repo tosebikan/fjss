57a38038113765b26c38a28d772c2582cfe507ff
tosebikan <tosebikan@gmail.com>
2021-08-10 12:58:04 +0000

    add productio end point

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:1337";
+// PRODUCTION
+export const BASE_URL = "https://fjss-admin.herokuapp.com/";
+
+// STAGING
+// export const BASE_URL = "http://localhost:1337";
