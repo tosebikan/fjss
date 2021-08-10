2ce181cc897470b0258ad56b195c154fb38c5874
tosebikan <tosebikan@gmail.com>
2021-08-10 12:55:04 +0000

    add api functions

---
+++ src/helpers/api.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { BASE_URL } from "./config";
+
+const apiFunctions = {
+  getCountDown: async () => get("/countdown"),
+  getProjects: async () => get("/projects")
+};
+
+const get = path => {
+  try {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${BASE_URL}/${path}`, {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(result => {
+          resolve(result.data);
+        })
+        .catch(error => {
+          console.log("api error>", error.response);
+          reject({
+            status: 500,
+            message: error.response
+          });
+        });
+
+      setTimeout(
+        () =>
+          reject({
+            status: 500,
+            msg: "Connection error, try again later"
+          }),
+        18000
+      );
+    });
+  } catch (err) {}
+};
+
+const post = (path, fd) => {
+  try {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(`${BASE_URL}/${path}`, fd, {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(result => {
+          resolve(result.data);
+        })
+        .catch(error => {
+          console.log("POST ERROR>", error.response);
+
+          reject({
+            status: 500,
+            msg: error.response
+          });
+        });
+
+      setTimeout(
+        () =>
+          reject({
+            status: 500,
+            msg: "Connection error, please try again later"
+          }),
+        18000
+      );
+    });
+  } catch (err) {}
+};
+
+module.exports = apiFunctions;
