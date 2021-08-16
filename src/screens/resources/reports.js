c66d155b2ae8be7ce3ffa1d16208f2b06e318120
tosebikan <tosebikan@gmail.com>
2021-08-16 08:19:15 +0000

    finish resource

--- src/screens/resources/reports.js
+++ src/screens/resources/reports.js
@@ -1,17 +1,52 @@
+import * as React from "react";
 import { saveAs } from "file-saver";
 import "./reports.css";
 import { FiChevronDown } from "react-icons/fi";
 import HeroImage from "../../assets/images/home_hero.jpg";
-import framework from "../../assets/pdfs/Framework-for-Justice-Reforms-in-Scotland-July-2020.pdf";
+// import framework from "../../assets/pdfs/Framework-for-Justice-Reforms-in-Scotland-July-2020.pdf";
+import { apiFunctions } from "../../helpers/api";
+import { Link } from "react-router-dom";
 
 function Reports() {
+  const [reports, setReports] = React.useState([]);
+  const [currReport, setCurrReports] = React.useState(0);
   const download = e => {
     e.preventDefault();
-    const blob = new Blob([framework], {
-      type: "application/pdf;charset=utf-8"
-    });
-    saveAs(blob, "Framework-for-Justice-Reforms-in-Scotland-July-2020.pdf");
+    //   const blob = new Blob([framework], {
+    //     type: "application/pdf;charset=utf-8"
+    //   });
+    //   saveAs(blob, "Framework-for-Justice-Reforms-in-Scotland-July-2020.pdf");
   };
+  React.useEffect(() => {
+    fetchReports();
+  }, []);
+
+  const fetchReports = async () => {
+    let res = await apiFunctions.getReports();
+    setReports(res);
+
+    console.log("report", res);
+  };
+
+  const save = (url, title) => {
+    // saveAs(url, title);
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        // setFetching(false);
+        const blobURL = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = blobURL;
+        a.style = "display: none";
+
+        if (title && title.length) a.download = title;
+        document.body.appendChild(a);
+        a.click();
+      })
+      .catch(err => console.log(err));
+  };
+
+  let currentReport = reports[currReport];
   return (
     <div className="reports_container">
       <div className="policy_hero">
@@ -24,16 +59,17 @@ function Reports() {
 
       <div className="reports_download">
         <div className="report_group">
-          <h2>The Framework for Justice Reform in Scotland July 2020</h2>
+          <h2>{currentReport?.title}</h2>
           <FiChevronDown className="report_group_icon" />
         </div>
-        {/*<a
-          href="../../assets/pdfs/Framework-for-Justice-Reforms-in-Scotland-July-2020.pdf"
-          download="Framework-for-Justice-Reforms-in-Scotland-July-2020.pdf"
+        {/*<Link to={currentReport?.image.url} target="_blank" download>
+          Download
+        </Link>*/}
+        <button
+          onClick={() => save(currentReport?.image.url, currentReport?.title)}
         >
-          download
-        </a>*/}
-        <button onClick={download}>Downloads</button>
+          Downloads
+        </button>
       </div>
     </div>
   );
