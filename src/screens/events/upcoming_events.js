26e68a73f2019a8030b4c8a0961f8d52799b5b68
tosebikan <tosebikan@gmail.com>
2022-05-18 21:34:53 +0000

    fix upcoming events

--- src/screens/events/upcoming_events.js
+++ src/screens/events/upcoming_events.js
@@ -1,10 +1,11 @@
 import HeroImage from "../../assets/images/home_hero.jpg";
-import homeevent from "../../assets/images/homeevent.png";
-import { FaInstagram } from "react-icons/fa";
+import { IoTimeOutline, IoLocationSharp } from "react-icons/io5";
 import "./upcoming_events.css";
 import { events } from "../../helpers/data";
+import { Link } from "react-router-dom";
+
 function UpcomingEvents() {
-  let upcoming = events.filter(el => el.status === "upcoming");
+  let upcoming = events.filter(el => el.status === "pending");
   return (
     <div className="upcomingEvents_container">
       <div className="upcoming_events_hero">
@@ -24,28 +25,32 @@ function UpcomingEvents() {
             <div className="upcoming_events_card_left">
               <div>
                 <div className="upcoming_events_date">
-                  <p>13</p>
-                  <p className="upcoming_events_bold"> Apr </p>
+                <p className="upcoming_events_bold">{el?.startDate}</p>
                 </div>
                 <p className="upcoming_events_text">{el.title}</p>
                 <div className="row">
-                  <FaInstagram className="event_icon" />
-                  <p> 7:00 pm - 10:00 pm </p>{" "}
+                <IoTimeOutline className="event_icon" />
+                  <p>
+                    {" "}
+                    {el.startTime} - {el.endTime}{" "}
+                  </p>{" "}
                 </div>
                 <div className="row">
-                  <FaInstagram className="event_icon" /> <p>{el.type}</p>
+                <IoLocationSharp className="event_icon" /> <p>{el.type}</p>
                 </div>
               </div>
 
               <div>
+              <Link to={{ pathname: "/event-details", state: { el } }}>
                 <button className="upcoming_events_card_button">
                   Read more
                 </button>
+                </Link>
               </div>
             </div>
 
             <div className="upcoming_events_card_right">
-              <img src={homeevent} alt="" />
+              <img src={el?.image} alt="" />
             </div>
           </div>
         ))}
